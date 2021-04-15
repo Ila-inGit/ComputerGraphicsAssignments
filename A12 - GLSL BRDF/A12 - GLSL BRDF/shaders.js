a97692c092ce675ca6cc207e2eea8b73981db293
Ilaria <61754608+Ila-inGit@users.noreply.github.com>
2021-04-15 16:57:47 +0200

    assignment 13 done

--- A12 - GLSL BRDF/A12 - GLSL BRDF/shaders.js
+++ A12 - GLSL BRDF/A12 - GLSL BRDF/shaders.js
@@ -1,51 +1,51 @@
 function shaders() {
-// The shader can find the required informations in the following variables:
+  // The shader can find the required informations in the following variables:
 
-//vec3 fs_pos;		// Position of the point in 3D space
-//
-//float SpecShine;		// specular coefficient for both Blinn and Phong
-//float DToonTh;		// Threshold for diffuse in a toon shader
-//float SToonTh;		// Threshold for specular in a toon shader
-//
-//vec4 diffColor;		// diffuse color
-//vec4 ambColor;		// material ambient color
-//vec4 specularColor;	// specular color
-//vec4 emit;			// emitted color
-//	
-//vec3 normalVec;		// direction of the normal vecotr to the surface
-//vec3 eyedirVec;		// looking direction
-//
-//
-// Lighr directions can be found into:
-//vec3 lightDirA;
-//vec3 lightDirB;
-//vec3 lightDirC;
-//
-//and intensity is returned into:
-//
-//vec4 lightColorA;
-//vec4 lightColorB;
-//vec4 lightColorC;
-//
-// ambient light color contribution is returned into
-//
-// vec4 ambientLight;
-//
-// The final color computed from the shader should be inserted into variable
-//
-// vec4 out_color;
+  //vec3 fs_pos;		// Position of the point in 3D space
+  //
+  //float SpecShine;		// specular coefficient for both Blinn and Phong
+  //float DToonTh;		// Threshold for diffuse in a toon shader
+  //float SToonTh;		// Threshold for specular in a toon shader
+  //
+  //vec4 diffColor;		// diffuse color
+  //vec4 ambColor;		// material ambient color
+  //vec4 specularColor;	// specular color
+  //vec4 emit;			// emitted color
+  //
+  //vec3 normalVec;		// direction of the normal vecotr to the surface
+  //vec3 eyedirVec;		// looking direction
+  //
+  //
+  // Lighr directions can be found into:
+  //vec3 lightDirA;
+  //vec3 lightDirB;
+  //vec3 lightDirC;
+  //
+  //and intensity is returned into:
+  //
+  //vec4 lightColorA;
+  //vec4 lightColorB;
+  //vec4 lightColorC;
+  //
+  // ambient light color contribution is returned into
+  //
+  // vec4 ambientLight;
+  //
+  // The final color computed from the shader should be inserted into variable
+  //
+  // vec4 out_color;
 
-// Lambert diffuse and Ambient material. No specular or emisssion.
-var S1 = `
+  // Lambert diffuse and Ambient material. No specular or emisssion.
+  var S1 = `
 	vec4 LAcontr = clamp(dot(lightDirA, normalVec),0.0,1.0) * lightColorA;
 	vec4 LBcontr = clamp(dot(lightDirB, normalVec),0.0,1.0) * lightColorB;
 	vec4 LCcontr = clamp(dot(lightDirC, normalVec),0.0,1.0) * lightColorC;
 	out_color = clamp(diffColor * (LAcontr + LBcontr + LCcontr) + ambientLight * ambColor, 0.0, 1.0);
 `;
 
-// Lambert diffuse and Blinn specular. No ambient and emission.
-//DONE
-var S2 = `
+  // Lambert diffuse and Blinn specular. No ambient and emission.
+  //DONE
+  var S2 = `
 	vec4 LAcontr = clamp(dot(lightDirA, normalVec),0.0,1.0) * lightColorA;
 	vec4 LBcontr = clamp(dot(lightDirB, normalVec),0.0,1.0) * lightColorB;
 	vec4 LCcontr = clamp(dot(lightDirC, normalVec),0.0,1.0) * lightColorC;
@@ -59,9 +59,9 @@ var S2 = `
 							specularColor *(BspecA + BspecB + BspecC) , 0.0, 1.0);
 `;
 
-// Ambient and Phong specular. No emssion and no diffuse.
-// non funziona l'ambient...
-var S3 = `
+  // Ambient and Phong specular. No emssion and no diffuse.
+  // non funziona l'ambient...
+  var S3 = `
 
 	vec4 PspecA = pow(clamp(dot(eyedirVec, -reflect(lightDirA, normalVec) ), 0.0, 1.0),SpecShine) * lightColorA;
 	vec4 PspecB = pow(clamp(dot(eyedirVec, -reflect(lightDirB, normalVec) ), 0.0, 1.0),SpecShine) * lightColorB;
@@ -71,8 +71,8 @@ var S3 = `
 	out_color = clamp( specularColor * (PspecA + PspecB + PspecC) + ambientLight * ambColor , 0.0, 1.0);
 `;
 
-// Diffuse, ambient, emission and Phong specular.
-var S4 = `
+  // Diffuse, ambient, emission and Phong specular.
+  var S4 = `
 
 	vec4 LAcontr = clamp(dot(lightDirA, normalVec),0.0,1.0) * lightColorA;
 	vec4 LBcontr = clamp(dot(lightDirB, normalVec),0.0,1.0) * lightColorB;
@@ -87,47 +87,28 @@ var S4 = `
 							ambientLight * ambColor + emit , 0.0, 1.0);
 `;
 
-// Ambient, Toon diffuse and and Toon (Blinn based) specular. No emssion.
-//float DToonTh;		// Threshold for diffuse in a toon shader
-//float SToonTh;		// Threshold for specular in a toon shader
-var S5 = `
+  // Ambient, Toon diffuse and and Toon (Blinn based) specular. No emssion.
+  //float DToonTh;		// Threshold for diffuse in a toon shader
+  //float SToonTh;		// Threshold for specular in a toon shader
+  var S5 = `
+	
+	vec4 LAcontr = lightColorA * clamp( sign( dot( lightDirA, normalVec ) - DToonTh ),0.0,1.0);
+	vec4 LBcontr = lightColorB * clamp( sign( dot( lightDirB, normalVec ) - DToonTh ),0.0,1.0);
+	vec4 LCcontr = lightColorC * clamp( sign( dot( lightDirC, normalVec ) - DToonTh ),0.0,1.0);
+	
+	vec4 BspecA = lightColorA * 
+		clamp( sign ( dot( normalVec, normalize(lightDirA + eyedirVec) ) - SToonTh ), 0.0, 1.0);
+				
+	vec4 BspecB = lightColorB * 
+		clamp( sign ( dot( normalVec, normalize(lightDirB + eyedirVec) ) - SToonTh ), 0.0, 1.0);
 
-	vec4 LAcontr = diffColor;
-	vec4 LBcontr = diffColor;
-	vec4 LCcontr = diffColor;
+	vec4 BspecC = lightColorC *
+		clamp( sign ( dot( normalVec, normalize(lightDirC + eyedirVec) ) - SToonTh ), 0.0, 1.0);
 
-	if( clamp(dot(lightDirA, normalVec),0.0,1.0)  > DToonTh){
-		LAcontr = lightColorA;
-	}
 
-	if( clamp(dot(lightDirB, normalVec),0.0,1.0)  > DToonTh){
-		LBcontr = lightColorB;
-	}
-
-	if( clamp(dot(lightDirC, normalVec),0.0,1.0)  > DToonTh){
-		LCcontr = lightColorC;
-	}
-
-	vec4 BspecA = specularColor;
-	vec4 BspecB = specularColor;
-	vec4 BspecC = specularColor;
-
-	if( clamp(dot(normalVec, normalize(lightDirA + eyedirVec) ), 0.0, 1.0)  > SToonTh){
-		BspecA = lightColorA;
-	}
-
-	if( clamp(dot(normalVec, normalize(lightDirB + eyedirVec) ), 0.0, 1.0)  > SToonTh){
-		BspecB = lightColorB;
-	}
-
-	if( clamp(dot(normalVec, normalize(lightDirC + eyedirVec) ), 0.0, 1.0)  > SToonTh){
-		BspecC = lightColorC;
-	}
-
-	out_color = clamp( (BspecA + BspecB + BspecC) + 
-							(LAcontr + LBcontr + LCcontr) + ambientLight * ambColor , 0.0, 1.0);
+	out_color = clamp( specularColor * (BspecA + BspecB + BspecC) + 
+			diffColor * (LAcontr + LBcontr + LCcontr) + ambientLight * ambColor , 0.0, 1.0);
 `;
 
-	return [S1, S2, S3, S4, S5];
+  return [S1, S2, S3, S4, S5];
 }
-
--- A13 - Smooth objects/A13 - Smooth objects/models.js
+++ A13 - Smooth objects/A13 - Smooth objects/models.js
@@ -1,37 +1,247 @@
+//shpere
+const horizontalLines = 24;
+const verticalLines = 22;
+const radius = 3;
+var m, n;
+var i, j;
+//cilinder
+const sectorCount = 5;
+const height = 3;
+const cilinRadius = 2;
+
 function buildGeometry() {
-	// Draws a pyramid --- Already done, just for inspiration
-	var vert1 = [[0.0,1.0,0.0, 0.0, 0.4472,-0.8944],[ 1.0,-1.0,-1.0, 0.0, 0.4472,-0.8944],[-1.0,-1.0,-1.0, 0.0, 0.4472,-0.8944],
-				 [0.0,1.0,0.0, 0.8944, 0.4472,0.0],[ 1.0,-1.0, 1.0, 0.8944, 0.4472,0.0],[ 1.0,-1.0,-1.0, 0.8944, 0.4472,0.0], 
-				 [0.0,1.0,0.0, 0.0, 0.4472,0.8944],[-1.0,-1.0, 1.0, 0.0, 0.4472,0.8944],[ 1.0,-1.0, 1.0, 0.0, 0.4472,0.8944], 
-				 [0.0,1.0,0.0, -0.8944, 0.4472,0.0],[-1.0,-1.0,-1.0, -0.8944, 0.4472,0.0],[-1.0,-1.0, 1.0, -0.8944, 0.4472,0.0], 
-				 [-1.0,-1.0,-1.0, 0.0,-1.0,0.0],[1.0,-1.0,-1.0, 0.0,-1.0,0.0], [1.0,-1.0,1.0, 0.0,-1.0,0.0], [-1.0,-1.0,1.0, 0.0,-1.0,0.0],
-				];
-	var ind1 = [0, 1, 2,  3, 4, 5,  6, 7, 8,  9, 10, 11,  12, 13, 14,  12, 14, 15];
-	var color1 = [0.0, 0.0, 1.0];
-	addMesh(vert1, ind1, color1);
-	
-	// Draws a cube -- To do for the assignment.
-	var vert2 = [[-1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,1.0,0.0, 0.0, 0.0,1.0], [-1.0,1.0,0.0, 0.0, 0.0,1.0]];
-	var ind2 = [0, 1, 2,  0, 2, 3];
-	var color2 = [0.0, 1.0, 1.0];
-	addMesh(vert2, ind2, color2);
-	
-	// Draws function y = sin(x) * cos(z) with -3 <= x <= 3 and -3 <= z <= 3 -- To do for the assignment.
-	var vert3 = [[-1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,1.0,0.0, 0.0, 0.0,1.0], [-1.0,1.0,0.0, 0.0, 0.0,1.0]];
-	var ind3 = [0, 1, 2,  0, 2, 3];
-	var color3 = [0.0, 1.0, 1.0];
-	addMesh(vert3, ind3, color3);
-	
-	// Draws a Cylinder --- To do for the assignment
-	var vert4 = [[-1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,1.0,0.0, 0.0, 0.0,1.0], [-1.0,1.0,0.0, 0.0, 0.0,1.0]];
-	var ind4 = [0, 1, 2,  0, 2, 3];
-	var color4 = [1.0, 1.0, 0.0];
-	addMesh(vert4, ind4, color4);
+  // Draws a pyramid --- Already done, just for inspiration
+  var vert1 = [
+    [0.0, 1.0, 0.0, 0.0, 0.4472, -0.8944],
+    [1.0, -1.0, -1.0, 0.0, 0.4472, -0.8944],
+    [-1.0, -1.0, -1.0, 0.0, 0.4472, -0.8944],
+    [0.0, 1.0, 0.0, 0.8944, 0.4472, 0.0],
+    [1.0, -1.0, 1.0, 0.8944, 0.4472, 0.0],
+    [1.0, -1.0, -1.0, 0.8944, 0.4472, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.4472, 0.8944],
+    [-1.0, -1.0, 1.0, 0.0, 0.4472, 0.8944],
+    [1.0, -1.0, 1.0, 0.0, 0.4472, 0.8944],
+    [0.0, 1.0, 0.0, -0.8944, 0.4472, 0.0],
+    [-1.0, -1.0, -1.0, -0.8944, 0.4472, 0.0],
+    [-1.0, -1.0, 1.0, -0.8944, 0.4472, 0.0],
+    [-1.0, -1.0, -1.0, 0.0, -1.0, 0.0],
+    [1.0, -1.0, -1.0, 0.0, -1.0, 0.0],
+    [1.0, -1.0, 1.0, 0.0, -1.0, 0.0],
+    [-1.0, -1.0, 1.0, 0.0, -1.0, 0.0],
+  ];
+  var ind1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 12, 14, 15];
+  var color1 = [0.0, 0.0, 1.0];
+  addMesh(vert1, ind1, color1);
 
-	// Draws a Sphere --- To do for the assignment.
-	var vert5 = [[-1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,-1.0,0.0, 0.0, 0.0,1.0], [1.0,1.0,0.0, 0.0, 0.0,1.0], [-1.0,1.0,0.0, 0.0, 0.0,1.0]];
-	var ind5 = [0, 1, 2,  0, 2, 3];
-	var color5 = [1.0, 0.0, 0.0];
-	addMesh(vert5, ind5, color5);
+  // Draws a cube -- To do for the assignment.
+  var vert2 = [
+    [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0],
+    [1.0, -1.0, -1.0, 1.0, -1.0, -1.0],
+    [1.0, 1.0, -1.0, 1.0, 1.0, -1.0],
+    [-1.0, 1.0, -1.0, -1.0, 1.0, -1.0],
+    [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0],
+    [1.0, -1.0, 1.0, 1.0, -1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+    [-1.0, 1.0, 1.0, -1.0, 1.0, 1.0],
+  ];
+  var ind2 = [
+    0,
+    1,
+    3,
+    3,
+    1,
+    2,
+    1,
+    5,
+    2,
+    2,
+    5,
+    6,
+    5,
+    4,
+    6,
+    6,
+    4,
+    7,
+    4,
+    0,
+    7,
+    7,
+    0,
+    3,
+    3,
+    2,
+    7,
+    7,
+    2,
+    6,
+    4,
+    5,
+    0,
+    0,
+    5,
+    1,
+  ];
+  var color2 = [0.0, 1.0, 1.0];
+  addMesh(vert2, ind2, color2);
+
+  // Draws function y = sin(x) * cos(z) with -3 <= x <= 3 and -3 <= z <= 3 -- To do for the assignment.
+  var vert3 = [];
+  var ind3 = [];
+  var color3 = [0.0, 1.0, 1.0];
+  var count = 0;
+
+  for (i = 0; i <= 6; i++) {
+    for (j = 0; j <= 6; j++) {
+      x = i - 3;
+      z = j - 3;
+      vert3[i * 7 + j] = [
+        x,
+        Math.cos(z) * Math.sin(x),
+        z,
+        -Math.sin(z) * Math.sin(x),
+        1,
+        Math.cos(z) * Math.cos(x),
+      ];
+      //vert3[i * 7 + j] = [x, 0, z];
+    }
+  }
+
+  for (i = 0; i < 6; i++) {
+    for (j = 0; j < 6; j++) {
+      ind3[count] = i * 7 + j;
+      ind3[count + 1] = (i + 1) * 7 + j;
+      ind3[count + 2] = (i + 1) * 7 + j + 1;
+      ind3[count + 3] = i * 7 + j;
+      ind3[count + 4] = (i + 1) * 7 + j + 1;
+      ind3[count + 5] = i * 7 + j + 1;
+      count = count + 6;
+    }
+  }
+  addMesh(vert3, ind3, color3);
+
+  // Draws a Cylinder --- To do for the assignment
+  var vert4 = [];
+  var ind4 = [];
+  var color4 = [1.0, 1.0, 0.0];
+
+  let sectorSteps = (2 * Math.PI) / sectorCount;
+  let sectorAngle = 0;
+
+  var indexcil = 0;
+
+  vert4[indexcil] = [0, 0, 0, 0, 0, -1];
+  indexcil++;
+  for (j = 0; j <= 3; j++) {
+    for (i = 0; i <= sectorCount; i++) {
+      sectorAngle = i * sectorSteps;
+      x = Math.cos(sectorAngle) * cilinRadius;
+      y = Math.sin(sectorAngle) * cilinRadius;
+
+      if (j == 0) {
+        z = 0;
+        vert4[indexcil] = [x, y, z, 0, 0, -1];
+      } else if (j == 1) {
+        z = 0;
+        vert4[indexcil] = [
+          x,
+          y,
+          z,
+          Math.cos(sectorAngle),
+          Math.sin(sectorAngle),
+          0,
+        ];
+      } else if (j == 2) {
+        z = height;
+        vert4[indexcil] = [
+          x,
+          y,
+          z,
+          Math.cos(sectorAngle),
+          Math.sin(sectorAngle),
+          0,
+        ];
+      } else {
+        z = height;
+        vert4[indexcil] = [x, y, z, 0, 0, 1];
+      }
+
+      indexcil++;
+    }
+  }
+  vert4[indexcil] = [0, 0, height, 0, 0, 1];
+
+  var indexincides = 0;
+  for (i = 0; i <= 2; i++) {
+    for (j = 0; j <= sectorCount; j++) {
+      if (i == 0) {
+        ind4[indexincides] = 0;
+        ind4[indexincides + 1] = j + 2;
+        ind4[indexincides + 2] = j + 1;
+        indexincides = indexincides + 3;
+      } else if (i == 1) {
+        ind4[indexincides] = i * (sectorCount + 1) + j + 1;
+        ind4[indexincides + 1] = (i + 1) * (sectorCount + 1) + j + 2;
+        ind4[indexincides + 2] = (i + 1) * (sectorCount + 1) + j + 1;
+        ind4[indexincides + 3] = i * (sectorCount + 1) + j + 2;
+        ind4[indexincides + 4] = (i + 1) * (sectorCount + 1) + j + 2;
+        ind4[indexincides + 5] = i * (sectorCount + 1) + j + 1;
+        indexincides = indexincides + 6;
+      } else {
+        ind4[indexincides] = (i + 1) * (sectorCount + 1) + j + 1;
+        ind4[indexincides + 1] = (i + 1) * (sectorCount + 1) + j + 2;
+        ind4[indexincides + 2] = indexcil;
+        indexincides = indexincides + 3;
+      }
+    }
+  }
+  addMesh(vert4, ind4, color4);
+
+  // Draws a Sphere --- To do for the assignment.
+  var vert5 = [];
+  var ind5 = [];
+  var index = 0;
+
+  for (m = 0; m <= horizontalLines; m++) {
+    for (n = 0; n <= verticalLines; n++) {
+      x =
+        Math.sin((Math.PI * m) / horizontalLines) *
+        Math.cos((2 * Math.PI * n) / verticalLines) *
+        radius;
+      y = Math.cos((Math.PI * m) / horizontalLines) * radius;
+      z =
+        Math.sin((Math.PI * m) / horizontalLines) *
+        Math.sin((2 * Math.PI * n) / verticalLines) *
+        radius;
+
+      vert5[index] = [
+        x,
+        y,
+        z,
+        Math.sin((Math.PI * m) / horizontalLines) *
+          Math.cos((2 * Math.PI * n) / verticalLines),
+        Math.cos((Math.PI * m) / horizontalLines),
+        Math.sin((Math.PI * m) / horizontalLines) *
+          Math.sin((2 * Math.PI * n) / verticalLines) *
+          0,
+      ];
+      index++;
+    }
+  }
+
+  var indexincides = 0;
+  for (i = 0; i < horizontalLines; i++) {
+    for (j = 0; j < verticalLines; j++) {
+      ind5[indexincides] = i * (verticalLines + 1) + j;
+      ind5[indexincides + 1] = (i + 1) * (verticalLines + 1) + j;
+      ind5[indexincides + 2] = (i + 1) * (verticalLines + 1) + j + 1;
+      ind5[indexincides + 3] = i * (verticalLines + 1) + j;
+      ind5[indexincides + 4] = (i + 1) * (verticalLines + 1) + j + 1;
+      ind5[indexincides + 5] = i * (verticalLines + 1) + j + 1;
+      indexincides = indexincides + 6;
+    }
+  }
+
+  var color5 = [1.0, 0.0, 0.0];
+  addMesh(vert5, ind5, color5);
 }
-
