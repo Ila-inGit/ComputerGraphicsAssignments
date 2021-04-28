ce0fdd143973eaf4d23d84d43d96c84c96a4ff40
Ilaria <61754608+Ila-inGit@users.noreply.github.com>
2021-04-28 17:55:59 +0200

    assignment 16 done

--- A16 - Scene/A16 - Scene/scene.js
+++ A16 - Scene/A16 - Scene/scene.js
@@ -1,10 +1,34 @@
 function drawSceneTree(S) {
-	var i;
-	for(i = 0; i < S.length; i++) {
-		draw(i, utils.multiplyMatrices(utils.multiplyMatrices(utils.multiplyMatrices(
-			 utils.MakeTranslateMatrix(S[i][0], S[i][1], S[i][2]),
-			 utils.MakeRotateZMatrix(S[i][5])),
-			 utils.MakeRotateXMatrix(S[i][3])),
-			 utils.MakeRotateYMatrix(S[i][4])));
-	}
+  var i;
+  // disegno il palmo della mano
+  var mat0 = calc(S, 0);
+  draw(0, mat0);
+  // ciclo per tutte le dita
+  for (i = 1; i < 6; i++) {
+    var mat = calc(S, i);
+    // collego le prime falangi al palmo
+    mat = utils.multiplyMatrices(mat0, mat);
+    draw(i, mat);
+    //collego le seconde falangi alle prime
+    res = utils.multiplyMatrices(mat, calc(S, S[i][6]));
+    draw(S[i][6], res);
+    // controllo che non sia il pollice e che abbia un altra falange e nel caso la collego
+    if (S[S[i][6]][6] != -1) {
+      res = utils.multiplyMatrices(res, calc(S, S[S[i][6]][6]));
+      draw(S[S[i][6]][6], res);
+    }
+  }
+}
+
+function calc(S, i) {
+  return utils.multiplyMatrices(
+    utils.multiplyMatrices(
+      utils.multiplyMatrices(
+        utils.MakeTranslateMatrix(S[i][0], S[i][1], S[i][2]),
+        utils.MakeRotateZMatrix(S[i][5])
+      ),
+      utils.MakeRotateXMatrix(S[i][3])
+    ),
+    utils.MakeRotateYMatrix(S[i][4])
+  );
 }
