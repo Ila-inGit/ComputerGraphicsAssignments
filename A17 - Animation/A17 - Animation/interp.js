bb9a7684c8ed234ab34e6b3750917feae8caa3eb
Ilaria <61754608+Ila-inGit@users.noreply.github.com>
2021-05-17 15:09:41 +0200

    assignement 17 done

--- A17 - Animation/A17 - Animation/interp.js
+++ A17 - Animation/A17 - Animation/interp.js
@@ -24,17 +24,31 @@ function InterpMat(
   //
   // return the interpolated transform matrix with the given position and rotation
 
-  var q1 = Quaternion.fromEuler(rx1, ry1, rz1);
-  var q2 = Quaternion.fromEuler(rx2, ry2, rz2);
+  var q1 = Quaternion.fromEuler(
+    utils.degToRad(rz1),
+    utils.degToRad(rx1),
+    utils.degToRad(ry1)
+  );
+  var q2 = Quaternion.fromEuler(
+    utils.degToRad(rz2),
+    utils.degToRad(rx2),
+    utils.degToRad(ry2)
+  );
 
   q1 = q1.slerp(q2)(a);
 
   var matFromQuat = q1.toMatrix4();
 
-  var scaleMatrix = utils.MakeScaleMatrix(1);
-  var traslMatrix1 = utils.MakeTranslateMatrix(tx1 - tx2, ty1 - ty2, tz1 - tz2);
+  var rx = this.lerp(tx1, tx2, a);
+  var ry = this.lerp(ty1, ty2, a);
+  var rz = this.lerp(tz1, tz2, a);
 
-  var out = this.multiplyMatrices(matFromQuat, scaleMatrix);
-  out = this.multiplyMatrices(traslMatrix1, out);
+  var traslMatrix1 = utils.MakeTranslateMatrix(rx, ry, rz);
+
+  var out = utils.multiplyMatrices(traslMatrix1, matFromQuat);
   return out;
+}
+
+function lerp(a, b, alpha) {
+  return (1 - alpha) * a + alpha * b;
 }
